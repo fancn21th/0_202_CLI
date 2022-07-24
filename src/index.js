1e0fb81f71d1b534956eb2178baba8a80f171113
fancn21th <fancn21th@gmail.com>
2022-07-24 17:06:13 +0800

    parse - or -- flags

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 console.log(process.argv);
+
+const flags = [];
+
+process.argv.forEach((arg) => {
+  if (/^-/.test(arg)) {
+    flags.push(arg.replace(/^-{1,2}/, ""));
+  }
+});
+
+console.log(flags);
