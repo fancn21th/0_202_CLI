0629374abffbb6f1e09e6ef94a13682161b67db6
fancn21th <fancn21th@gmail.com>
2022-07-24 20:38:40 +0800

    what is capitcal of china

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./nodejs-fs.js";
-// import { askQuestion } from "./inquirer-3rd.js";
+// import "./nodejs-fs.js";
+import { askQuestion } from "./inquirer-3rd.js";
 // import { askQuestion } from "./nodejs-fs.js";
 
-// askQuestion();
+askQuestion();
--- src/inquirer-3rd.js
+++ src/inquirer-3rd.js
@@ -1,21 +1,30 @@
 import inquirer from "inquirer";
+import chalk from "chalk";
+import fs from "fs/promises";
 
 async function askQuestion() {
-  const answers = await inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "live",
-      message: "What do you live?",
-    },
-  ]);
+  const questions = await fs.readFile("./src/data/questions.json");
 
-  console.log("you name is:", answers.name);
-  console.log("you live in:", answers.live);
+  const parsedQuestions = JSON.parse(questions.toString());
+
+  const resolvedQuestions = parsedQuestions.map((question) => ({
+    name: question.question,
+    type: question.options ? "list" : "input",
+    choices: question.options,
+    message: question.question,
+  }));
+
+  const answers = await inquirer.prompt(resolvedQuestions);
+
+  console.log(answers);
+
+  parsedQuestions.forEach((question) => {
+    if (question.answer === answers[question.question]) {
+      console.log(chalk.green(`答案正确`));
+    } else {
+      console.log(chalk.red(`答案错误`));
+    }
+  });
 }
 
 export { askQuestion };
